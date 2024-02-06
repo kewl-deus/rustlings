2b2683755c3701e5a5bdc87a8566fc32b7e3baeb
kewl-deus <deus.mail@web.de>
2024-02-06 15:55:36 +0100

    vecs & move

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -3,19 +3,30 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
 
     let vec1 = fill_vec(vec0);
 
+    //println!("{}", join_comma(&vec0));
+    //let _x = vec0[0];
+
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
+fn join_comma(vec: &Vec<i32>) -> String {
+    println!("hi");
+    join(vec, ", ")
+}
+
+fn join(vec: &Vec<i32>, sep: &str) -> String {
+    vec.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(sep)
+}
+
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    //ownership move here!!!
+    let mut vec = vec;
 
     vec.push(88);
 
